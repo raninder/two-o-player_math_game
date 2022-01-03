3e1dce015679ed3c637e266e3feadad0bee6ac2b
raninder dhillon <raninderdhillon@gmail.com>
2022-01-03 13:40:21 -0500

    created question for players

---
+++ ques.rb
@@ -0,0 +1,17 @@
+class Question
+	attr_accessor :number1, :number2
+
+	def initialize
+		@number1 = rand(1..20)
+		@number2 = rand(1..20)
+	end
+
+	def put_question
+		puts "what does #{number1} plus #{number2} equal?"
+		sum = @number1 + @number2
+		puts "answer is #{sum}"
+	end
+
+end
+ q1 = Question.new
+ q1.put_question
